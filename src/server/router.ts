e6c0edbeab834b5c5f0b13a03d5a745ee94a1c52
xielingsheng <1550464586@qq.com>
2022-03-25 20:12:57 +0800

    添加了注释

--- src/server/router.ts
+++ src/server/router.ts
@@ -1,7 +1,7 @@
-import * as KoaRouter from 'koa-router'
+import * as KoaRouter from 'koa-router' //  引入koa-router
 import { dosql, getKey, recvData } from './utils'
 export const koaRouter = new KoaRouter()
-koaRouter.prefix('/api')
+koaRouter.prefix('/api') // koaRouter的所有路径都会自动被添加该(/api)路由前缀。
 koaRouter.post('/login', async ctx => {
   // const result = await new Promise((fn) => {
   //   connection.query(sql, [ctx.query.username, ctx.query.password], (err, brr) => {
@@ -17,9 +17,8 @@ koaRouter.post('/login', async ctx => {
   //     console.log(err, brr);
   //   });
   // });
-  const { username, password } = await recvData(ctx)
-  const dbResult = await dosql('SELECT * FROM `account` WHERE `userName` = ? ;', [username])
-  // console.log(dbResult);
+  const { username, password } = await recvData(ctx) // 接收前端传来的username和password，分别赋值username和password
+  const dbResult = await dosql('SELECT * FROM `account` WHERE `userName` = ? ;', [username]) // 把变量username等于数据库字段userName的记录全部找出来，并赋值给dbResult
   if (dbResult.length === 0) {
     ctx.body = '用户名不存在'
     return
@@ -28,21 +27,22 @@ koaRouter.post('/login', async ctx => {
   else if (dbResult[0].password !== password) {
     ctx.body = '密码错误'
   } else {
-    const key = getKey(username)
+    const key = getKey(username) // 把username进行加密
     ctx.cookies.set('key', key)
-    ctx.cookies.set('username', username)
+    ctx.cookies.set('username', username) //  在上下文中写入cookiecookie相当于通行证，这里是把用户的变量username往cookie里设置username,
     ctx.body = '登录成功'
   }
 })
 
 koaRouter.post('/register', async ctx => {
+  // 使用loaRouter的post方法去接收http请求并调用对应的函数。如router.('/path',async fn)或者router.post('/path',async fn)。
   type user = {
     username: string
     password: string
     sex: string
     email: string
   }
-  const post: user = await recvData(ctx)
+  const post: user = await recvData(ctx) // post的类型就是user的类型，就是说赋值给post的话要以user的形式
   const inser = await dosql('INSERT ignore INTO `account`(`userName`, `password`, `sex`, `email`) VALUES (?,?,?,?)', [
     post.username,
     post.password,
@@ -50,6 +50,7 @@ koaRouter.post('/register', async ctx => {
     post.email,
   ])
   if (inser.affectedRows === 0) {
+    // 是数据库中的唯一索引限制了不能重复插入数据，所以可以用影响行数是否为0来判断是否插入成功，如果影响行数为0返回客户端注册失败。
     ctx.body = '注册失败'
   } else {
     ctx.body = '注册成功'
--- src/server/utils.ts
+++ src/server/utils.ts
@@ -10,7 +10,9 @@ const connection = mysql.createConnection({
   database: 'movie',
 })
 connection.connect()
-export const getKey = (username: string) => crypto.createHash('md5').update(username).digest('hex')
+/** 计算第一个入参的MD5并返回加密后字符串 */
+export const getKey = (username: string): string => crypto.createHash('md5').update(username).digest('hex')
+/** 第一个参数带占位符的sql语句，第二个参数是代替占位符的数组，返回一个记录结果数组或者受影响的行数，记录结果数组是由key（字段）和value（数值）的对象（记录）构成数组 */
 export const dosql = (sql: string, item: (string | number)[]): Promise<any[] & OkPacket> => {
   return new Promise(fn => {
     connection.query(sql, item, (err, brr) => {
@@ -22,9 +24,10 @@ export const dosql = (sql: string, item: (string | number)[]): Promise<any[] & O
     })
   })
 }
+/** 服务端监听接收客户端的请求数据，把接收的数据用内存块数据存储起来在拼接成一整个内存块，转换成字符串，再以对象的形式返回 */
 export const recvData = (ctx: Koa.ParameterizedContext): Promise<any> =>
   new Promise(fn => {
-    const body: Buffer[] = []
+    const body: Buffer[] = [] // 缓冲区合并。在处理像TCP流或文件流时，必须使用到二进制数据。因此在 Node.js中，定义了一个 Buffer 类，该类用来创建一个专门存放二进制数据的缓存区。Buffer 库为 Node.js 带来了一种存储原始数据的方法，可以让 Node.js 处理二进制数据，每当需要在 Node.js 中处理I/O操作中移动的数据时，就有可能使用 Buffer 库。
     ctx.req.on('data', chuck => body.push(chuck))
     ctx.req.on('end', () => fn(JSON.parse(String(Buffer.concat(body)))))
   })
