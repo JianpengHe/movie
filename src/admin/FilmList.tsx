6828ca00fe85938075940a83afc401d5b58f7beb
JianpengHe <i@hejianpeng.cn>
2022-04-01 02:22:37 +0800

    20220401

--- src/admin/FilmList.tsx
+++ src/admin/FilmList.tsx
@@ -1,43 +1,87 @@
-import { useRequest } from 'ahooks'
-import { Button, Form, Input, Table, Typography } from 'antd'
-import { ColumnsType } from 'antd/lib/table'
+import { useBoolean, useRequest } from 'ahooks'
+import { Button, Card, Form, Input, Rate, Table, Tooltip, Typography, Image, Tag, Space, Popconfirm } from 'antd'
+import { PlusOutlined, UserOutlined } from '@ant-design/icons'
 import React, { useEffect } from 'react'
 import { ajax } from './ajax'
 import './FilmList.less'
-const { Title } = Typography
+import FilmForm from './FilmForm'
 interface IProp {}
 const FilmList: React.FC<IProp> = () => {
   //  React.FC提供了类型检查和自动完成的静态属性,简单来说，不知道用什么组件类型时，就用 React.FC
-  const { run, data } = useRequest(ajax('get', '/filmList'), { manual: true })
+
+  const [url, setUrl] = React.useState('')
+  const [filmInfo, setFilmInfo] = React.useState<any>(undefined)
+
+  const { run, data } = useRequest(ajax.get('/filmList'), {
+    manual: true,
+    onSuccess(obj) {
+      setFilter(obj)
+    },
+  })
+
+  const { run: del } = useRequest(ajax.delete('/film'), {
+    manual: true,
+    onSuccess() {
+      run()
+    },
+  })
+
+  const [filter, setFilter] = React.useState<any[]>([])
   useEffect(() => {
     run()
   }, [])
-  const columns: ColumnsType<any> = [
-    { title: '电影名', dataIndex: 'fName', key: 'fName', fixed: 'left' },
-    { title: 'id', dataIndex: 'fid', key: 'fid' },
-    { title: '评分', dataIndex: 'score', key: 'score' },
-    { title: '时长', dataIndex: 'filmlong', key: 'filmlong' },
-    { title: '上映时间', dataIndex: 'releaseTime', key: 'releaseTime' },
-    { title: '基准票价', dataIndex: 'price', key: 'price' },
-    { title: '简介', dataIndex: 'introduce', key: 'introduce', ellipsis: true },
-    { title: '海报', dataIndex: 'fImage', key: 'fImage', ellipsis: true },
-    { title: '演员', dataIndex: 'actor', key: 'actor', ellipsis: true },
-    { title: '类型', dataIndex: 'type', key: 'type' },
-    { title: '票房', dataIndex: 'totalBoxoffice', key: 'totalBoxoffice' },
+  const colorList = [
+    'magenta',
+    'red',
+    'volcano',
+    'orange',
+    'gold',
+    'lime',
+    'green',
+    'cyan',
+    'blue',
+    'geekblue',
+    'purple',
   ]
-  console.log(data)
+  const colorMap: Map<string, string> = new Map()
+  ;[...new Set((filter || []).map(({ type }) => type.replace(/\s/g, '').split(',')).flat())].forEach((tag, index) =>
+    colorMap.set(tag as string, colorList[index % colorList.length])
+  )
+
   return (
     <>
-      <div className="title">
-        <Title level={5}>影讯信息管理</Title>
-        <Button type="primary">上架电影</Button> {/*主按钮primary：用于主行动点，一个操作区域只能有一个主按钮。*/}
-      </div>
-      <div className="context">
-        <Form layout="inline">
-          {/*表单布局为inline*/}
+      <Image
+        style={{ display: 'none' }}
+        src={url}
+        preview={{
+          visible: url !== '',
+          src: url,
+          onVisibleChange(value) {
+            if (value) {
+              return
+            }
+            setUrl('')
+          },
+        }}
+      />
+      <Card
+        title="影讯信息管理"
+        extra={
+          <Button icon={<PlusOutlined />} type="primary" onClick={() => setFilmInfo({})}>
+            上架电影
+          </Button>
+        }
+      >
+        <Form
+          onFinish={({ fName }) => {
+            setFilter(data.filter(({ fName: oldName }) => oldName.indexOf(fName) >= 0))
+          }}
+          onReset={() => setFilter(data)}
+          layout="inline"
+        >
           <div className="space-between" style={{ marginBottom: '24px' }}>
             <div>
-              <Form.Item label="电影名称">
+              <Form.Item label="电影名称" name="fName">
                 <Input />
               </Form.Item>
             </div>
@@ -53,8 +97,169 @@ const FilmList: React.FC<IProp> = () => {
             </div>
           </div>
         </Form>
-        <Table dataSource={(data as any)?.data} columns={columns} scroll={{ x: 2300, y: 600 }} />;
-      </div>
+        <Table
+          rowKey="fid"
+          dataSource={filter}
+          columns={[
+            { title: '电影名', dataIndex: 'fName', key: 'fName', fixed: 'left', width: 120, align: 'center' },
+            {
+              title: '类型',
+              dataIndex: 'type',
+              key: 'type',
+              render(value) {
+                return value
+                  .replace(/\s/g, '')
+                  .split(',')
+                  .map((tag, index) => (
+                    <p key={index}>
+                      <Tag color={colorMap.get(tag)}>{tag}</Tag>
+                    </p>
+                  ))
+              },
+              width: 60,
+              align: 'center',
+            },
+            {
+              title: '上映时间',
+              dataIndex: 'releaseTime',
+              key: 'releaseTime',
+              render(value) {
+                return value.substring(0, 10)
+              },
+              width: 120,
+              ellipsis: true,
+              align: 'center',
+              defaultSortOrder: 'ascend',
+              sorter: (a, b) => new Date(a.releaseTime).getTime() - new Date(b.releaseTime).getTime(),
+            },
+            {
+              title: '时长',
+              dataIndex: 'filmlong',
+              key: 'filmlong',
+              width: 90,
+              ellipsis: true,
+              render(value) {
+                return `${value}分钟`
+              },
+              align: 'center',
+            },
+
+            {
+              title: '基准票价',
+              dataIndex: 'price',
+              key: 'price',
+              width: 100,
+              ellipsis: true,
+              render(value) {
+                return `￥${value / 100}`
+              },
+              align: 'center',
+            },
+            {
+              title: '评分',
+              dataIndex: 'score',
+              key: 'score',
+              width: 120,
+              align: 'center',
+              render(value) {
+                return (
+                  <Tooltip placement="top" title={`${value / 10}分`}>
+                    <div>
+                      <Rate allowHalf disabled defaultValue={value / 20} />
+                    </div>
+                  </Tooltip>
+                )
+              },
+            },
+
+            {
+              title: '简介',
+              dataIndex: 'introduce',
+              key: 'introduce',
+              align: 'center',
+              render(value) {
+                return (
+                  <Tooltip title={value} placement="topLeft">
+                    <Typography.Paragraph ellipsis={{ rows: 4 }} style={{ textAlign: 'left', textIndent: '2em' }}>
+                      {value}
+                    </Typography.Paragraph>
+                  </Tooltip>
+                )
+              },
+            },
+            {
+              title: '海报',
+              dataIndex: 'fImage',
+              key: 'fImage',
+              render(value) {
+                return <Image width={60} src={value} preview={{ src: value.substring(0, value.lastIndexOf('_')) }} />
+              },
+              align: 'center',
+              width: 80,
+            },
+            {
+              title: '演员',
+              dataIndex: 'actor',
+              key: 'actor',
+              width: 200,
+              render(value) {
+                return (
+                  <Typography.Paragraph ellipsis={{ rows: 4, expandable: true }}>
+                    {value.map(({ name, headimg }, index) => (
+                      <Tag
+                        icon={<UserOutlined />}
+                        key={index}
+                        onClick={() => setUrl(headimg)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        {name}
+                      </Tag>
+                    ))}
+                  </Typography.Paragraph>
+                )
+              },
+            },
+
+            {
+              title: '票房',
+              dataIndex: 'totalBoxoffice',
+              key: 'totalBoxoffice',
+              render(value) {
+                const arr = [...String(value)]
+                return arr
+                  .map((n, i) => n + ((arr.length - 1 - i) % 3 === 0 && i !== arr.length - 1 ? ',' : ''))
+                  .join('')
+              },
+              width: 120,
+              align: 'center',
+            },
+            {
+              title: '操作',
+              dataIndex: 'fid',
+              key: 'fid',
+              fixed: 'right',
+              align: 'center',
+              width: 120,
+              render(fid, info) {
+                return (
+                  <Space>
+                    <Button type="primary" size="small" onClick={() => setFilmInfo({ ...info })}>
+                      编辑
+                    </Button>
+                    <Popconfirm placement="topRight" title="是否确定下架" onConfirm={() => del('fid=' + fid)}>
+                      <Button type="primary" danger size="small">
+                        下架
+                      </Button>
+                    </Popconfirm>
+                  </Space>
+                )
+              },
+            },
+          ]}
+          scroll={{ x: 1500 }}
+        />
+      </Card>
+      <FilmForm filmInfo={filmInfo} setFilmInfo={setFilmInfo} />
     </>
   )
 }
--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -10,14 +10,12 @@ import {
   BarChartOutlined,
   PoweroffOutlined,
 } from '@ant-design/icons' // 在前端定义枚举映射
-import styled from 'styled-components'
 import FilmList from './FilmList'
 import HallList from './HallList'
-import './app.css'
+import './app.less'
 import OrderList from './OrderList'
 import UserList from './UserList'
 const { Header, Sider, Content } = Layout
-const Div = styled.div``
 const menu: {
   name: string
   icon: React.ReactNode
@@ -54,63 +52,59 @@ export default () => {
   const [page, setPage] = React.useState('0')
 
   return (
-    <Div>
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-          {' '}
-          {/*使用自定义触发器，可以设置 trigger={null} 来隐藏默认设定*/}
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" onSelect={({ key }) => setPage(key)} defaultSelectedKeys={[page]}>
-            {menu.map(({ name, icon }, index) => (
-              <Menu.Item key={String(index)} icon={icon}>
-                {name}
-              </Menu.Item>
-            ))}
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            <Row justify="space-between">
-              {/* 栅格化系统基于 Flex 布局，允许子元素在父节点内的水平对齐方式 */}
-              <Col span={8}>
-                {/* 通过 row 在水平方向建立一组 column（简写 col）。 你的内容应当放置于 col 内，并且，只有 col 可以作为
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        {/*使用自定义触发器，可以设置 trigger={null} 来隐藏默认设定*/}
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" onSelect={({ key }) => setPage(key)} defaultSelectedKeys={[page]}>
+          {menu.map(({ name, icon }, index) => (
+            <Menu.Item key={String(index)} icon={icon}>
+              {name}
+            </Menu.Item>
+          ))}
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="header">
+          <Row justify="space-between">
+            {/* 栅格化系统基于 Flex 布局，允许子元素在父节点内的水平对齐方式 */}
+            <Col span={8}>
+              {/* 通过 row 在水平方向建立一组 column（简写 col）。 你的内容应当放置于 col 内，并且，只有 col 可以作为
                 row 的直接元素。 栅格系统中的列是指 1 到 24 的值来表示其跨越的范围。例如，三个等宽的列可以使用来创建。 */}
-                <Col span={8} />
-                {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                  className: 'trigger',
-                  onClick() {
-                    setCollapsed(!collapsed) //  鼠标点击触发器菜单栏展开（不是收起状态）
-                  },
-                })}
-              </Col>
-              <Col span={8} offset={8} style={{ textAlign: 'right', margin: '0 20px', fontSize: '18px' }}>
-                {/* //offset栅格左侧的间隔格数，间隔内不可以有栅格 */}
-                <Space>
-                  {/* 设置组件之间的间距。避免组件黏在一起，拉开一定距离，适合行内元素的水平间距 */}
-                  <a>
-                    <UserOutlined />
-                    admin
-                  </a>
-                  <a>
-                    <PoweroffOutlined />
-                    退出登录
-                  </a>
-                </Space>
-              </Col>
-            </Row>
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: '24px 16px',
-              // padding: 24,
-              minHeight: 280,
-            }}
-          >
-            {menu[Number(page)].render}
-          </Content>
-        </Layout>
+              <Col span={8} />
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick() {
+                  setCollapsed(!collapsed) //  鼠标点击触发器菜单栏展开（不是收起状态）
+                },
+              })}
+            </Col>
+            <Col span={8} offset={8} style={{ textAlign: 'right', margin: '0 20px', fontSize: '18px' }}>
+              {/* //offset栅格左侧的间隔格数，间隔内不可以有栅格 */}
+              <Space>
+                {/* 设置组件之间的间距。避免组件黏在一起，拉开一定距离，适合行内元素的水平间距 */}
+                <a>
+                  <UserOutlined />
+                  admin
+                </a>
+                <a>
+                  <PoweroffOutlined />
+                  退出登录
+                </a>
+              </Space>
+            </Col>
+          </Row>
+        </Header>
+        <Content
+          style={{
+            padding: '24px 16px',
+            overflow: 'auto',
+            minHeight: 280,
+          }}
+        >
+          {menu[Number(page)].render}
+        </Content>
       </Layout>
-    </Div>
+    </Layout>
   )
 }
