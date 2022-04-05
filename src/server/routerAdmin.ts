775ab06525127cb08dff7fa13139230ad7fd5a6f
JianpengHe <i@hejianpeng.cn>
2022-04-05 19:35:46 +0800

    20220405 1935

--- src/server/routerAdmin.ts
+++ src/server/routerAdmin.ts
@@ -131,3 +131,129 @@ koaRouterAdmin.post('/play', async ctx => {
 
   ctx.body = '成功'
 })
+
+type IAutoFilmInfo = {
+  fid: number
+  filmlong: number
+  totalBoxoffice: number
+  releaseDays: number
+  price: number
+  maxScale?: number
+  minScale?: number
+}
+
+koaRouterAdmin.get('/autoPlay', async ctx => {
+  const { cid } = ctx.query
+  const filmInfoList: IAutoFilmInfo[] = await dosql(
+    'SELECT fid,filmlong,totalBoxoffice,datediff(now(),releaseTime) as releaseDays,price FROM `film` ORDER BY `film`.`totalBoxoffice` DESC LIMIT 10',
+    []
+  )
+  const scaleTotal = filmInfoList.reduce(
+    (sum, { releaseDays, totalBoxoffice }) => sum + totalBoxoffice / releaseDays,
+    0
+  )
+  filmInfoList.forEach(obj => {
+    const scale = obj.totalBoxoffice / obj.releaseDays / scaleTotal
+    obj.maxScale = Math.min(1, scale + 0.1)
+    obj.minScale = Math.max(0, scale - 0.1)
+  })
+  const halls = await dosql('SELECT hid,capacity,price FROM `hall` WHERE cid=?', [String(cid)])
+  // console.log(filmInfoList)
+  console.time('time')
+  const hallNum = 1 / halls.length
+  const qu5 = (num: number) => Math.ceil((num / 5) * 5)
+
+  const plans = halls.map(({ capacity }) =>
+    autoPlay(
+      filmInfoList.map(({ filmlong }) => qu5(filmlong + capacity / 2)),
+      qu5(960 + capacity / 2)
+    ).filter(plan => {
+      const total = plan.reduce((a, b) => a + b)
+      return !plan.some((times, index) => times / total > hallNum + ((filmInfoList[index] || {}).maxScale || 0))
+    })
+  )
+  const result: number[][][] = []
+  const hallIndexArr: number[][] = []
+  const dfs = (hallIndex: number) => {
+    if (hallIndex >= plans.length) {
+      const total = hallIndexArr.flat().reduce((a, b) => a + b) // 该计划总排片电影数量
+      if (
+        hallIndexArr[0].every((_, filmIndex) => {
+          const filmTotalScale = hallIndexArr.map(hallPlan => hallPlan[filmIndex]).reduce((a, b) => a + b) / total // 某部电影所有影厅的排片比例
+          return (
+            // @ts-ignore
+            filmTotalScale <= filmInfoList[filmIndex].maxScale && filmTotalScale >= filmInfoList[filmIndex].minScale
+          )
+        })
+      ) {
+        result.push([...hallIndexArr])
+      }
+      // console.log(hallIndexArr)
+    } else {
+      plans[hallIndex].forEach(plan => {
+        hallIndexArr[hallIndex] = plan
+        dfs(hallIndex + 1)
+      })
+    }
+  }
+  dfs(0)
+
+  console.log(result)
+  console.timeEnd('time')
+  ctx.body = '成功'
+  // const halls = await dosql('SELECT hid,capacity,price FROM `hall` WHERE cid=?', [String(cid)])
+})
+
+// const autoPlay = (filmInfoList: IAutoFilmInfo[], freeTime: number, duration = 960) => {
+//   const result: IAutoFilmInfo[][] = []
+//   const sum: IAutoFilmInfo[] = []
+//   let usedTime = 0
+//   const dfs = (filmInfo: IAutoFilmInfo) => {
+//     sum.push(filmInfo)
+//     usedTime += filmInfo.filmlong
+//     //  console.log(usedTime)
+//     if (usedTime > duration) {
+//       result.push([...sum].slice(0, sum.length - 1))
+//     } else {
+//       usedTime += freeTime
+//       filmInfoList.forEach(dfs)
+//       usedTime -= freeTime
+//     }
+//     usedTime -= filmInfo.filmlong
+//     sum.pop()
+//   }
+//   filmInfoList.forEach(dfs)
+//   return result
+// }
+
+const autoPlay = (filmInfoList: IAutoFilmInfo['filmlong'][], duration = 960) => {
+  const lastFilmLong = filmInfoList.pop() || 0
+  const result: number[][] = []
+  const sum: number[] = []
+  let usedTime = 0
+  const dfs = (filmIndex: number) => {
+    if (filmIndex >= filmInfoList.length) {
+      result.push([
+        ...sum,
+        Math.floor(
+          (duration -
+            filmInfoList.reduce((previousValue, filmlong, index) => previousValue + filmlong * sum[index], 0)) /
+            lastFilmLong
+        ),
+      ])
+    } else {
+      const maxTime = Math.floor(duration / filmInfoList[filmIndex])
+      for (let i = 0; i <= maxTime; i++) {
+        usedTime += filmInfoList[filmIndex] * i
+        if (usedTime <= duration) {
+          sum[filmIndex] = i
+          dfs(filmIndex + 1)
+        }
+        usedTime -= filmInfoList[filmIndex] * i
+      }
+    }
+  }
+  dfs(0)
+  // console.log(result.map(arr => arr.reduce((a, b, i) => a + b * (filmInfoList[i] || lastFilmLong), 0)))
+  return result
+}
